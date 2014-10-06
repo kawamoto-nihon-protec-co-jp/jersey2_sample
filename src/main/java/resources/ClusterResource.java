cca4299f120526995832c81b71753d4355525f03
kawamoto-nihon-protec-co-jp <kawamoto@nihon-protec.co.jp>
2014-10-07 01:54:47 +0900

    commit

--- src/main/java/resources/ClusterResource.java
+++ src/main/java/resources/ClusterResource.java
@@ -1,6 +1,6 @@
 package resources;
 
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -9,22 +9,22 @@ import core.Cluster;
 @Path("/admin")
 public class ClusterResource {
 
-    @POST
+//    @GET
+//    @Path("{data}")
+//    public String getCluster(@PathParam("data") String data) {
+//        System.out.println("kawamoto----"+ data);
+//        Cluster c = new Cluster();
+//        c.setId(Integer.valueOf(data));
+//        c.setName("GF Cluster");
+//        return "POST DATA RECEIVED. data[]";
+//    }
+
+    @GET
     @Path("{id}")
-    public String getCluster(@PathParam("id") String id) {
-        System.out.println("kawamoto----"+id);
+    public Cluster getCluster(@PathParam("id") String id) {
         Cluster c = new Cluster();
         c.setId(Integer.valueOf(id));
         c.setName("GF Cluster");
-        return "POST DATA RECEIVED. data[]";
+        return c;
     }
-
-//    @GET
-//    @Path("{id}")
-//    public Cluster getCluster(@PathParam("id") String id) {
-//        Cluster c = new Cluster();
-//        c.setId(Integer.valueOf(id));
-//        c.setName("GF Cluster");
-//        return c;
-//    }
 }
